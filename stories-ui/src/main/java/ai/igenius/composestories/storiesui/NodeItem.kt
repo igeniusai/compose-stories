3c3c523a9bc565df188dc810a3c0ad77692a06b6
Luigi Vitelli <luigi.vitelli@igenius.ai>
2023-09-22 14:47:42 +0200

    icon animation

--- stories-ui/src/main/java/ai/igenius/composestories/storiesui/NodeItem.kt
+++ stories-ui/src/main/java/ai/igenius/composestories/storiesui/NodeItem.kt
@@ -2,10 +2,15 @@ package ai.igenius.composestories.storiesui
 
 import ai.igenius.composestories.storiesui.models.FolderNode
 import ai.igenius.composestories.storiesui.models.TreeNode
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationDrawerItem
@@ -13,7 +18,10 @@ import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,19 +29,17 @@ fun NodeItem(
     model: NodeItemModel,
     onClick: () -> Unit,
 ) {
-    val icon = when (model.node) {
-        is FolderNode -> {
-            if(model.isOpen) Icons.Default.KeyboardArrowUp
-            else Icons.Default.ArrowDropDown
-        }
-        else -> Icons.Default.Build
-    }
+    val angle by animateFloatAsState(if (model.isOpen) 90f else 0f, label = "icon_rotation")
     NavigationDrawerItem(
-        icon = { Icon(icon, contentDescription = null) },
-        label = {
-            Text(
-                model.node.title,
+        icon = model.icon?.let { {
+            Icon(
+                imageVector = it,
+                contentDescription = null,
+                modifier = Modifier.rotate(angle)
             )
+        } },
+        label = {
+            Text(model.node.title)
         },
         selected = model.selected,
         onClick = onClick,
@@ -48,5 +54,8 @@ data class NodeItemModel(
     val node: TreeNode,
     val rootDistance: Int,
     val selected: Boolean,
-    val isOpen: Boolean,
-)
+    val isOpen: Boolean
+) {
+    val icon: ImageVector? get() =
+        if(node is FolderNode) Icons.AutoMirrored.Filled.KeyboardArrowRight else null
+}
